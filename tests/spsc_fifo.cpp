63664fd64caf625c2342147d6ee8ee14795e68fd
Bob Beaty <drbob@TheManFromSPUD.com>
2012-03-11 21:17:02 -0500

    Added the SPSC Circular FIFO Tests
    I needed to make an initial cut of the SPSC Circular FIFO class, so I did
    that by doing groups of 500 pushes and pops. This verifies the data popped
    as well as the sizes, so it's all looking correct in a simple single-threaded
    test.

---
+++ tests/spsc_fifo.cpp
@@ -0,0 +1,79 @@
+/**
+ * This is the tests for the SPSC CircularFIFO
+ */
+//	System Headers
+#include <iostream>
+#include <string>
+
+//	Third-Party Headers
+
+//	Other Headers
+#include "spsc/CircularFIFO.h"
+#include "util/timer.h"
+
+int main(int argc, char *argv[]) {
+	bool	error = false;
+
+	// make a circular FIFO or 1024 int32_t - max
+	dkit::spsc::CircularFIFO<int32_t, 10>	q;
+	// put 500 values on the queue - and check the size
+	if (!error) {
+		std::cout << "=== Testing speed and correctness of CircularFIFO ===" << std::endl;
+
+		// get the starting time
+		uint64_t	goTime = dkit::util::timer::usecSinceEpoch();
+
+		// do this often enough to rotate through the size of the values
+		int32_t		trips = 500000;
+		for (int32_t cycle = 0; cycle < trips; ++cycle) {
+			// put 500 values on the queue - and check the size
+			for (int32_t i = 0; i < 500; ++i) {
+				if (!q.push(i)) {
+					error = true;
+					std::cout << "ERROR - could not push the value " << i << std::endl;
+					break;
+				}
+			}
+			// now check the size
+			if (!error) {
+				if (q.size() != 500) {
+					error = true;
+					std::cout << "ERROR - pushed 500 integers, but size() reports only " << q.size() << std::endl;
+				} else {
+					if (cycle == 0) {
+						std::cout << "Passed - pushed on 500 integers" << std::endl;
+					}
+				}
+			}
+			// pop off 500 integers and it should be empty
+			if (!error) {
+				int32_t		v = 0;
+				for (int32_t i = 0; i < 500; ++i) {
+					if (!q.pop(v) || (v != i)) {
+						error = true;
+						std::cout << "ERROR - could not pop the value " << i << std::endl;
+						break;
+					}
+				}
+			}
+			// now check the size
+			if (!error) {
+				if (!q.empty()) {
+					error = true;
+					std::cout << "ERROR - popped 500 integers, but size() reports " << q.size() << std::endl;
+				} else {
+					if (cycle == 0) {
+						std::cout << "Passed - popped all 500 integers" << std::endl;
+					}
+				}
+			}
+		}
+
+		// get the elapsed time
+		goTime = dkit::util::timer::usecSinceEpoch() - goTime;
+		std::cout << "Passed - did " << (trips * 500) << " push/pop pairs in " << (goTime/1000.0) << "ms = " << (goTime/(trips * 500.0)) << "us/op" << std::endl;
+	}
+
+	std::cout << (error ? "FAILED!" : "SUCCESS") << std::endl;
+	return (error ? 1 : 0);
+}
